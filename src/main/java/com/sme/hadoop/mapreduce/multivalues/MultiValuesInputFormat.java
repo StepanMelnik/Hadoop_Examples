dae779a648c686b59bb243b0662fa3f4301c5a2e
stepan <stepan.melnik@viskan.se>
2020-10-17 14:30:45 +0300

    InputFormat does not split a small test file

--- src/main/java/com/sme/hadoop/mapreduce/multivalues/MultiValuesInputFormat.java
+++ src/main/java/com/sme/hadoop/mapreduce/multivalues/MultiValuesInputFormat.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.CombineFileInputFormat;
@@ -23,6 +24,12 @@ public class MultiValuesInputFormat extends CombineFileInputFormat<MultiValuesWo
     public RecordReader<MultiValuesWordOffset, Text> createRecordReader(InputSplit split, TaskAttemptContext context) throws IOException
     {
         return new CombineFileRecordReader<>((CombineFileSplit) split, context, CombineFileLineRecordReader.class);
+    }
+
+    @Override
+    protected boolean isSplitable(JobContext context, Path file)
+    {
+        return false;
     }
 
     /**
